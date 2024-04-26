5bfa58b2fc65f74865c4a4f21501ede5e7826821
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-26 12:35:45 +0200

    init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from '../simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'; //
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+let galleryList = '';
+
+galleryItems.forEach(image => {
+  galleryList += `
+    <li>
+        <a href="${image.original}" class="gallery__item">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}">
+        </a>
+    </li>`;
+});
+
+gallery.innerHTML = galleryList;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
