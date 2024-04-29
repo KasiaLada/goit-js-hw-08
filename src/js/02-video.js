3d7504dc0233a6fb2e8e2da30464dea48b3be70e
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-29 12:21:56 +0200

    Zrobiono zad. 2/8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(Number.parseFloat(currentTime));
+
+const delayedUptdate = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', eventData => {
+  delayedUptdate(eventData);
+});
