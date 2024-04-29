849fb07a9be1d5382dcf492a89361a71bf5d8d1a
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-29 13:21:59 +0200

    Zrobiono zad. 3/8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+// Funkcja do zapisywania stanu formularza w localStorage
+const saveFormData = () => {
+  localStorage.setItem(feedbackFormData, JSON.stringify(formData));
+};
+
+// Opóźnienie funkcji zapisującej dane do local storage
+const throttledSaveFormData = throttle(saveFormData, 500);
+
+// Ustawienie wartości formularza przy ładowaniu strony
+const populateForm = () => {
+  const savedData = localStorage.getItem(feedbackFormData);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    form.elements.email.value = parsedData.email || '';
+    form.elements.message.value = parsedData.message || '';
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+  }
+};
+
+form.addEventListener('input', ev => {
+  formData[ev.target.name] = ev.target.value;
+  throttledSaveFormData();
+});
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  localStorage.removeItem(feedbackFormData);
+  form.reset();
+});
+
+populateForm();
